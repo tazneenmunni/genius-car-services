61e65956070a7acc287f15c6e48454e7ae6b18ee
Munni26 <tazneen99@gmail.com>
2022-04-12 22:15:17 +0600

    module 61 start

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 const Footer = () => {
     const today = new Date();
     const year = today.getFullYear();
@@ -7,6 +8,7 @@ const Footer = () => {
         <div>
             {/* <p>Copy right  © {new Date().getFullYear()} </p> */}
             <p className='text-center mt-5'>Copy right © {year}</p>
+
         </div>
     );
 };
